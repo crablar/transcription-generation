c0d5ff917b02d8d0a6e1cf3e7c8a373f5e6cb17b
Jeff Meyerson <meff.jeyerson@gmail.com>
2017-12-30 12:12:53 -0800

    script updated

--- main_script.sh
+++ main_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 NUM_TRANSCRIPTIONS_TO_MAKE=1
-
-sh check_for_finished_transcriptions.sh
+STORAGE_BUCKET_NAME=flac-for-transcription  
 
 for i in `seq 1 $NUM_TRANSCRIPTIONS_TO_MAKE`;
 do
+    sh check_for_finished_transcriptions.sh
     line=$(head -n 1 slug_mp3_pairs.txt)
     IFS=', ' read -r -a vars <<< "$line"
     slug=${vars[0]}
@@ -17,4 +17,8 @@ do
     ffmpeg -i ${slug}stereo.flac -ac 1 $slug.flac
     rm $slug.mp3
     rm ${slug}stereo.flac
+    
+    gsutil cp $slug.flac gs://flac-for-transcription/$slug.flac
+    node recognize async-gcs gs://$STORAGE_BUCKET_NAME/$slug.flac -e FLAC -r 44100 > ./finished_transcripts/$slug.txt
+    rm $slug.flac
 done
